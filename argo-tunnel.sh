60c5f96220a21265f9639eb4783c209c78fe93d1
varuntirumala1 <varun.tirumala@gmail.com>
2021-02-23 22:33:23 -0500

    Update argo-tunnel.sh

--- argo-tunnel.sh
+++ argo-tunnel.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cloudflared --origincert /data/cert.pem --config /data/argo.yml tunnel run bitwarden
+cloudflared --origincert /data/cert.pem --config /data/argo.yml tunnel run -p http2 bitwarden
